0acdef7594ba86b014de09ce4817d4cd08243981
Joseph Vazquez <josephvschool0469@gmail.com>
2024-10-22 21:01:07 -0700

    Up to data StartGame from home pc
    This is a updated V of how the final thing kind of looks like

--- StartGame.java
+++ StartGame.java
@@ -1,4 +1,28 @@
 public class StartGame {
     public static void main(String[] args) {
+
+        // Game obj connections
+        GameScripts ScriptOBJ = new GameScripts();
+        GameText TextOBJ = new GameText();
+
+        // intro
+        System.out.println("\n" + ScriptOBJ.introCard() + "\n");
+        GameScripts.introCheck();
+        GameScripts.mainMenuCheck();
+
+        // starting
+        GameScripts.type_100(TextOBJ.startText());
+        GameScripts.readyCheck();
+
+        // game play
+        GamePlay.main();
+
+        // ending
+        GameScripts.type_100(TextOBJ.endTextFast1());
+        GameScripts.type_300(TextOBJ.endTextSlow());
+        GameScripts.type_100(TextOBJ.endTextFast2());
+        GameScripts.type_100(ScriptOBJ.credits());
+
     }
+
 }
